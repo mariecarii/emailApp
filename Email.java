76a15c3b4ab3b82a21691360eb05a1174037413c
Marie Cari <mcari@nyc.yearup.org>
2022-12-10 22:48:53 -0500

    created constructor that asks user for first and last name

---
+++ Email.java
@@ -0,0 +1,27 @@
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //Create a constructor that takes in employee's first and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED" + this.firstName + this.lastName);
+
+        // call a method that asks for the department and returns it
+    }
+    //Ask for the department
+
+
+    //Generate a random password
+
+    //Set alternate email
+
+
+    //Change password
+
+}
