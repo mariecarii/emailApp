673a6880aac4bc326e0ccf19dba9753356b97a68
Marie Cari <mcari@nyc.yearup.org>
2022-12-10 22:58:19 -0500

    created method that takes in department

--- Email.java
+++ Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -13,8 +15,27 @@ public class Email {
         System.out.println("EMAIL CREATED" + this.firstName + this.lastName);
 
         // call a method that asks for the department and returns it
+        this.department = setDepartment();
+        System.out.println("Department Selected: " + this.department);
     }
     //Ask for the department
+    private String setDepartment() {
+        System.out.println("Enter the number associated to your department: \n 1 for Engineering \n 2 for Sales \n 3 for Accounting");
+        Scanner scan = new Scanner(System.in);
+        int depChoice = scan.nextInt();
+        if (depChoice == 1) {
+            return "Engineering";
+        }
+        else if (depChoice == 2) {
+            return "Sales";
+        }
+        else if (depChoice == 3) {
+            return "Accounting";
+        }
+        else {
+            return "Your response was invalid.";
+        }
+    }
 
 
     //Generate a random password
